a87de1c25aa4ff01ef0f4368e8495c068917decf
khushiG08 <khushigadhiya92@gmail.com>
2022-12-21 10:57:18 +0530

    21-12-2022

---
+++ assigment/module 1.c
@@ -0,0 +1,22 @@
+Q-1 what is software?what is software engineering?
+Ans: Software- It is a set of program.Software engineering is defined as a process of analyzing user requirements and then designing, building, and testing software application which will satisfy those requirements.
+
+Q-2 Explain types of software
+Ans: 2 Types of software
+
+(i) System software
+(ii) Application sofware
+
+Q-3 What is SDLC? Explain each phase of SDLC
+Ans: Software Development Life Cycle (SDLC) is a framework that defines activities that are performed during the software development process.
+
+1. Requirement: In this phase, all the requirements are collected from the customer/client. They are provided in a document called Businessmen requirement specification (BRS) and System requirement specification (SRS). All the details are discussed with the customer/client in detail. 
+2. Analysis: The analysis includes gathering all the specific details required for a new system as well as determining the first ideas for prototypes.
+3. Design: It has two steps:
+High-level design (HLD): It gives the architecture of software products.
+Low-level design (LLD): It describes how each and every feature in the product should work and every component.
+4.Development: The development stage is the part where developers actually write code and build the application according to the earlier design documents and outlined specifications.
+5. Testing: Testing is carried out to verify the entire system. The aim of the tester is to find out the gaps and defects within the system and also to check whether the system is running according to the requirement of the customer/client. 
+6. Deployment: After successful testing, the product is delivered/deployed to the client, and even clients are trained on how to use the product.
+7. Maintenance: Once the product has been delivered to the client a task of maintenance starts as when the client will come up with an error the issue should be fixed from time to time.     
+
---
+++ classwork/C programming/21-12-22/test1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+void main()
+{
+	printf("FName : Khushi\n");
+	printf("lName : Gadhiya\n");
+	printf("Address : Surat\n");
+	printf("D.O.B : 8 Nov 1997\n");
+	printf("Mo : 123456789");
+}
