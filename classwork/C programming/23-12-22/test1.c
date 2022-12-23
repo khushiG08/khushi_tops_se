95ac609645bebdad3c5087f210315bd55b7c25d4
khushiG08 <khushigadhiya92@gmail.com>
2022-12-23 10:56:48 +0530

    23-12-22

---
+++ classwork/C programming/23-12-22/test1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main()
+{
+	int no = 20;
+	char ch = 'A';
+	float pr = 52.35;
+	double price = 800035.25689;
+	char name[10] = "khushi";
+	char mobile[10] = "456321789";
+	
+	printf("%d\n",no);
+	printf("%c\n",ch);
+	printf("%.2f\n",pr);
+	printf("%lf\n",price);
+	printf("%s\n",name);
+	printf("%s",mobile);
+}
